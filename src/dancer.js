6323cfdee4744142c24f38cc184ee9fbfc0ab923
Hack Reactor Students <jakobharclerode@gmail.com>
2014-06-09 16:14:28 -0700

    refactoring to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -1,33 +1,23 @@
-// Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
+var MakeDancer = function(top, left, timeBetweenSteps) {
 
-  var dancer = {};
+  //creating a span tag to contain dancers
+  this.$node = $('<span class="dancer"></span>');
 
-  // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
-
-
-  dancer.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
+  this.step = function() {
+    setTimeout(this.step, timeBetweenSteps);
   };
-  dancer.step();
+  this.step();
 
-  dancer.setPosition = function(top, left){
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
+  this.setPosition = funtion(top, left) {
     var styleSettings = {
       top: top,
       left: left
     };
-    dancer.$node.css(styleSettings);
+    this.$node.css(styleSettings);
   };
 
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  this.setPosition(top, left);
 
-  return dancer;
+  return this;
+
 };
